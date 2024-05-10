bd9a1c83161ff0dc8df7f5ce86a973b5275c27c5
Alek Pachemski 201530 <alek.pachemski@students.finki.ukim.mk>
2024-05-10 22:21:16 +0200

    Added EventListener for timestamps

--- src/main/java/com/cloudgarden/discordbot/CloudGarden.java
+++ src/main/java/com/cloudgarden/discordbot/CloudGarden.java
@@ -1,2 +1,58 @@
-package com.cloudgarden.discordbot;public class CloudGarden {
+package com.cloudgarden.discordbot;
+
+
+import com.cloudgarden.discordbot.listeners.EventListener;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+public class CloudGarden {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    /**
+     * Loads environment variables and builds the bot shard manager.
+     * @throws LoginException occurs when bot token is invalid.
+     */
+    public CloudGarden() throws LoginException {
+        // Load environment variables
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+
+        // Build shard manager
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.watching("TechnoVisionTV"));
+        shardManager = builder.build();
+
+        // Register listeners
+        shardManager.addEventListener(new EventListener());
+    }
+
+    /**
+     * Retrieves the bot environment variables.
+     * @return the DotEnv instance for the bot.
+     */
+    public Dotenv getConfig() { return config; }
+
+    /**
+     * Retrieves the bot shard manager.
+     * @return the ShardManager instance for the bot.
+     */
+    public ShardManager getShardManager() { return shardManager; }
+
+    /**
+     * Main method where we start our bot.
+     */
+    public static void main(String[] args) {
+        try {
+            CloudGarden bot = new CloudGarden();
+        } catch (LoginException e) {
+            System.out.println("ERROR: Provided bot token is invalid!");
+        }
+    }
 }
